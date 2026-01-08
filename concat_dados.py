5ce5378d3f47c4a9cb815185f0257456e18aa790
Vandilma <vandilma_c@outlook.com>
2026-01-08 11:48:12 -0300

    add

---
+++ concat_dados.py
@@ -0,0 +1,9 @@
+## dois dados diferente de usuarios e concatenar em uma unica string
+
+info1 = input("Digite a primeira informação: ")
+info2 = input("Digite a segunda informação: ")
+
+resultado = info1 + " " + info2
+
+print("Informações concatenadas:", resultado)
+
---
+++ rept_textos.py
@@ -0,0 +1,5 @@
+texto = input("Digite um texto: ")
+quantidade = int(input("Digite um número inteiro: "))
+
+resultado = texto * quantidade
+print(resultado)
